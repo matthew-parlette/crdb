bd08c83b71542a632be90bc9ddb071e194d49e1f
Matt Parlette <matthew.parlette@gmail.com>
2014-02-04 09:18:37 -0500

    Implemented Customer: new, create, and show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,15 +2,37 @@ class CustomersController < ApplicationController
   before_filter :get_customers
   
   def index
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @customers }
+    end
   end
   
   def show
+    find_customer
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @customer }
+    end
   end
   
   def new
+    @customer = Customer.new
   end
   
   def create
+    @customer = Customer.new(filtered_params)
+    
+    respond_to do |format|
+      if @customer.save
+        flash[:notice] = 'Customer was successfully created.'
+        format.html { redirect_to @customer }
+        format.json { render json: @customer, status: :created }
+      else
+        format.html { render @customer.errors }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
   def edit
@@ -24,5 +46,13 @@ class CustomersController < ApplicationController
   
   private
     def get_customers
+    end
+    
+    def filtered_params
+      params.require(:customer).permit(:title,:name,:description)
+    end
+    
+    def find_customer
+      @customer = Customer.find(params[:id])
     end
 end
