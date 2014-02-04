8b7e3ac056ea1fdc18bf4f17876becc49330ae9b
Matt Parlette <matthew.parlette@gmail.com>
2014-02-04 08:58:35 -0500

    Skeleton for customers and projects controllers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,28 @@
 class CustomersController < ApplicationController
+  before_filter :get_projects
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+    def get_projects
+    end
 end
