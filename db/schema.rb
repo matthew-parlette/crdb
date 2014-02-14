3fd2fd4f11ab882be190447503739870586fdac7
Matt Parlette <matthew.parlette@gmail.com>
2014-02-14 15:47:41 -0500

    Added migration for links as string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211124416) do
+ActiveRecord::Schema.define(version: 20140214204455) do
 
   create_table "customers", force: true do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140211124416) do
     t.integer  "customer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "links",          default: ""
   end
 
   add_index "projects", ["customer_id"], name: "index_projects_on_customer_id"
