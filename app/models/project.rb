4621d99a3ebaf0e8de7077f51696ab22682ce0c6
Matt Parlette <matthew.parlette@gmail.com>
2014-02-15 09:55:26 -0500

    Adding links support in model and controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,16 @@ class Project < ActiveRecord::Base
   belongs_to :customer
   validates :title, presence: true
   validates :status, presence: true
+  
+  private
+    #format_links: ensure the links field is a comma-separated list.
+    def presave
+      #split on newline and comma
+      self.links = self.links.split(/[\n,]/).join(",")
+    end
+    
+    def preload
+      #convert comma-separated links to a list
+      self.links = self.links.split(",")
+    end
 end
