758a867eef66c6f67e1973494428e1d21a362eeb
Matt Parlette <matthew.parlette@gmail.com>
2014-02-15 17:30:51 -0500

    Update completed_date if status changes to complete
    Closes #28

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,21 @@
 class Project < ActiveRecord::Base
+  include ActiveModel::Dirty
+  
   belongs_to :customer
+  
+  before_save :update_completed_date
   
   validates :title, presence: true
   validates :status, presence: true
 
+  private
+    
+    #update the completed_date if the status was changed to completed
+    def update_completed_date
+      if self.status_changed?
+        if self.status == 'complete'
+          self.completed_date = Date.today
+        end
+      end
+    end
 end
