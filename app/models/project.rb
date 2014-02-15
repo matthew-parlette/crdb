72355ccf6b3f4f7216ac7da5a8942c8c3f1e0645
Matt Parlette <matthew.parlette@gmail.com>
2014-02-15 17:13:19 -0500

    Cleanup

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,16 +3,5 @@ class Project < ActiveRecord::Base
   
   validates :title, presence: true
   validates :status, presence: true
-  
-  private
-    #format_links: ensure the links field is a comma-separated list.
-    def presave
-      #split on newline and comma
-      self.links = self.links.split(/[\n,]/).join(",")
-    end
-    
-    def preload
-      #convert comma-separated links to a list
-      self.links = self.links.split(",")
-    end
+
 end
