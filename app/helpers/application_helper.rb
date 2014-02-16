c8d2cf7f470de9785e8bc4c7cdecbf938926eff1
Matt Parlette <matthew.parlette@gmail.com>
2014-02-16 17:30:38 -0500

    Added application helper for status options.
    Fixes #31

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def status_options
+    options = ['new',
+               'in progress',
+               'waiting on customer',
+               'on hold',
+               'cancelled',
+               'delivered',
+               'complete'
+              ]
+    return options
+  end
 end
