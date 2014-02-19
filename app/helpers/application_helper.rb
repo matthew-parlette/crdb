fa523f4262f81eca5a73bf5a292ad3b898f9d499
Matt Parlette <matthew.parlette@gmail.com>
2014-02-19 14:44:22 -0500

    Added helper function to return an ordered project list.
    Project list has entries for :active, :inactive, and :closed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,22 @@ module ApplicationHelper
               ]
     return options
   end
+  
+  #take a list of projects and organize them by:
+  # active, inactive, closed
+  def ordered_projects(project_list)
+     projects = {:active => nil, :inactive => nil, :closed => nil}
+     
+     project_list.each do |project|
+          if project.status.in?(['waiting on customer','on hold','delivered'])
+               projects[:inactive].push(project)
+          elsif project.status.in?(['cancelled','complete'])
+               projects[:closed].push(project)
+          else
+               projects[:active].push(project)
+          end
+     end
+     
+     return projects
+  end
 end
