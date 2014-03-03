f22f7a94e5c561fb5a87bff26d6c61174be71c32
Matt Parlette <matthew.parlette@gmail.com>
2014-03-03 11:04:16 -0500

    Added script to bump version

---
+++ bump_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+version="$1"
+info_color="\e[96m"
+reset_color="\e[0m"
+
+info () {
+  echo -e "$info_color$1$reset_color"
+}
+
+if [ -z "$version" ]; then
+  echo "version must be provided, use $0 v1.0"
+  exit 1
+fi
+
+info "bumping version to $version..."
+eval "sed -i 's/v[0-9]\.[0-9]/$version/g' app/views/layouts/application.html.erb"
+
+info "verify version is correct..."
+eval "git diff HEAD"
+info "to update git:"
+info "\t\$ git tab -a $version -m \"$version\""
+info "\t\$ git push --tags"
