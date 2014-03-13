3219310703ac7357ca291853d5da3463bcd3e11f
Matt Parlette <matthew.parlette@gmail.com>
2014-03-13 19:29:11 -0400

    Added planning task above project table for user direction

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ include ApplicationHelper
 
 class HomeController < ApplicationController
   before_filter :print_params
+  before_filter :load_today
   after_filter :format_flash
   
   def index
@@ -11,5 +12,15 @@ class HomeController < ApplicationController
   private
     def print_params
       print params
+    end
+
+    def load_today
+      # Create session variables if they don't already exist
+      session[:today] = {} if !session.has_key?("today")
+      session[:today][:projects] = [] if !session[:today].has_key?("projects")
+    end
+
+    def today_add_project(project_id)
+      session[:today][:projects].push(project_id)
     end
 end
