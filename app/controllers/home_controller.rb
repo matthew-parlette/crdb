f61efadc1f4d51347cca5ceecb2be19fed37c50a
Matt Parlette <matthew.parlette@gmail.com>
2014-03-14 07:25:56 -0400

    Reset today hash if the current hash is not for today.
    This sets a date field in the today hash, when the date changes, the today hash will be reset.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   before_filter :print_params
   before_filter :load_today
   after_filter :format_flash
+  after_filter :print_today_hash
   
   def index
     @projects = ordered_projects(Project.all.sort_by{|l| l[:title]})
@@ -11,12 +12,18 @@ class HomeController < ApplicationController
   
   private
     def print_params
-      print params
+      puts params
+    end
+
+    def print_today_hash
+      puts "\n\nToday hash: " + session[:today].to_s
     end
 
     def load_today
+      date_today = Time.zone.today.iso8601
       # Create session variables if they don't already exist
-      session[:today] = {} if !session.has_key?("today")
+      session[:today] = {:date => date_today} if !session.has_key?("today")
+      session[:today] = {:date => date_today} if session[:today][:date] != date_today
       session[:today][:projects] = [] if !session[:today].has_key?("projects")
     end
 
